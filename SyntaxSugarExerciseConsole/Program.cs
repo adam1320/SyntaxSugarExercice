fb874b87a579c3f2c6046a36c437bf9c3fd9522d
Adam Brannon <adam1320@rocketmail.com>
2022-01-17 13:18:11 -0500

    initial commit

---
+++ SyntaxSugarExerciseConsole/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace SyntaxSugarExerciseConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //Inferred Typing example
+
+            var answer = 4;
+            
+            string response;
+
+            if (answer < 9)
+            {
+                response = answer + "is less than nine";
+            }
+            else
+            {
+                response = answer + "greater than or equal to nine";
+            }
+
+            //String Interpolation example
+
+            int answer = 4;
+
+            string response;
+
+            if (answer < 9)
+            {
+                response = ($"{answer} is less than nine");
+            }
+            else
+            {
+                response = ($"{answer} greater than or equal to");
+            }
+
+            //Ternary Operator example
+
+            int answer = 4;
+            string response;
+             response = (answer) < 9 ? "answer + is less than nine" : answer + "greater than or equal to";
+        
+            
+
+
+
+
+        }
+    }
+}
